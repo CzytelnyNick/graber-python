5af7d4318abecbab191aeb83f1feccda165da040
CzytelnyNick <81709876+CzytelnyNick@users.noreply.github.com>
2023-03-28 15:00:17 +0200

    Add files via upload

--- graber.py
+++ graber.py
@@ -17,11 +17,30 @@ time.sleep(2)
 
 elem = driver.find_element(By.ID, "bigCookie")
 while(True):
+    
     elem = driver.find_element(By.ID, "bigCookie")
-
+    elem2 = driver.find_element(By.ID, "bigCookie")
+    prod = driver.find_element(By.ID, "product0")
+   
     elem.send_keys(Keys.RETURN)
-    elem2 = driver.find_element(By.XPATH, "//*[@id='cookies']")
-    print(elem2.get_property("value"))
+    prodValue = driver.find_element(By.ID, "productOwned0")
+    
+    if(prod.get_attribute("class") == "product unlocked enabled"):
+            # print()
+            driver.find_element(By.ID, "product0").click()
+            
+            if int(prodValue.text) == 10:
+                time.sleep(10)
+                prod = driver.find_element(By.ID, "product1")
+                prodValue = driver.find_element(By.ID, "productOwned1")
+
+            
+    
+    # i = 0;  
+    # elem2 = driver.find_element(By.CLASS_NAME, "")
+        
+        
+    # print(elem2.get_attribute("innerHTML")[0])
     # elem =
     # if elem.
 assert "No results found." not in driver.page_source
